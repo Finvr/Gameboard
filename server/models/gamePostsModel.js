55eef3ba64aa5cd9fb01fe3363703afce40bce46
Nathan Leonard <n.leonard668@gmail.com>
2015-09-23 12:30:04 -0500

    server-models-formatting

--- server/models/gamePostsModel.js
+++ server/models/gamePostsModel.js
@@ -1,34 +1,29 @@
 var db = require('../db.js');
 
-
 module.exports = {
 
 	getAll: function (){
 		return db.select()
-		.from('gameposts')
-		.then(function (gameposts){
-			console.log(gameposts);
-			return gameposts;
-		})
-		.catch(function(err){
-			console.log(err);
-			return err;
-		})
+		  .from('gameposts')
+		  .then(function (gameposts){
+			  console.log(gameposts);
+			  return gameposts;
+		  })
+		  .catch(function(err){
+			  console.log(err);
+			  return err;
+		  })
 
 	},
 	create: function(gamepost){
 		return db('gameposts').insert(gamepost)
-		.then(function(gamepost){
-           console.log(gamepost);
-            return gamepost;
-        })
-        .catch(function(err){
-           console.log(err);
-           return err;
-        })
+		  .then(function(gamepost){
+        console.log(gamepost);
+        return gamepost;
+      })
+      .catch(function(err){
+        console.log(err);
+        return err;
+      })
 	},
-	}
-	//module.exports.create({game_location:'test',game:'test',player_count:4, gamepost_description:'test'  });
-	module.exports.getAll();
-
-
+}
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,22 +3,18 @@ var db = require ('../db.js')
 module.exports = {
 
 create: function (facebookId, username){
-  return db('users').insert({facebook_id:facebookId, username:username})
-  .then(function(user){
-    console.log(user);
-    return user;
-  })
-  .catch(function(err){
-    console.log(err);
-    return err;
-  })
-}
-
-
-
-
-
-
-
-
+  return db('users')
+    .insert({
+      facebook_id:facebookId, 
+      username:username
+    })
+    .then(function(user){
+      console.log(user);
+      return user;
+    })
+    .catch(function(err){
+      console.log(err);
+      return err;
+    })
+  },
 }
