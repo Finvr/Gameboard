4c6273e3c02aa8537d2021c11a1285d8b3a51c23
Nathan Leonard <n.leonard668@gmail.com>
2015-09-23 15:56:09 -0500

    server-nl-update formatting

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var parse = require('body-parser');
-var Utils = require('./utils/helpers.js');
+var express = require('express'),
+    parse   = require('body-parser'),
+    Utils   = require('./utils/helpers.js');
 
 var app = express();
 
@@ -8,33 +8,34 @@ app.use(parse.urlencoded({extended: true}));
 app.use(parse.json());
 app.use(express.static(__dirname + '/../client'));
 
+//User Routes
 app.post('/users', function (req, res){
-	Utils.createUser(req, res);
-	
+  Utils.createUser(req, res);
 })
 
 app.get('/users/*', function (req, res){
-	res.send('get one user')
+  res.send('get one user')
 })
 
 app.put('/users/*',function (req, res){
-	res.send('update a user')
+  res.send('update a user')
 })
 
 app.delete('/users/*', function (req, res){
-	res.send('delete a user')
+  res.send('delete a user')
 })
 
 app.get('/users/*/games', function(req, res){
-	res.send('get the games for a specific user')
+  res.send('get the games for a specific user')
 })
 
+//GamePosts Routes
 app.get('/gameposts', function(req, res){
-	Utils.getAllGameposts(req, res);
+  Utils.getAllGameposts(req, res);
 })
 
 app.post('/gameposts', function (req, res){
-	Utils.createGamepost(req, res);
+  Utils.createGamepost(req, res);
 })
 
 
--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,41 +1,45 @@
-var Users= require ('../models/userModel.js');
-var Gameposts= require ('../models/gamePostsModel.js');
+var Users     = require ('../models/userModel.js'),
+    GamePosts = require ('../models/gamePostsModel.js');
 
 module.exports = {
-	getAllGameposts: function (req, res){
-		//autentication
-		Gameposts.getAll()
-		.then(function (data){
-			res.send(data);
-		})
-		.catch(function(err){
-			console.log(err);
-			res.send(err.message);
-		})
-	},
-	createGamepost: function (req, res){
-		var gamepost = req.body;
-		Gameposts.create(gamepost)
-		.then(function (data){
-			res.send(data);
-		})
-		.catch(function(err){
-			console.log(err);
-			res.send(err.message);
-		})
-	},
-	createUser: function (req, res){
-		//authentication
-		var testId = 250;
-		var username = req.body.username;
+  
+  getAllGameposts: function (req, res){
+    //autentication
+    GamePosts.getAll()
+      .then(function (data){
+        res.send(data);
+      })
+      .catch(function(err){
+        console.log(err);
+        res.send(err.message);
+      })
+  },
 
-		Users.create(testId, username)
-		.then(function (userId){
-			res.send(userId);
-		})
-		.catch(function(err){
-			console.log(err);
-			res.send(err.message);
-		})
-	}
+  createGamepost: function (req, res){
+    var gamepost = req.body;
+    
+    GamePosts.create(gamepost)
+      .then(function (data){
+        res.send(data);
+      })
+      .catch(function(err){
+        console.log(err);
+        res.send(err.message);
+      })
+  },
+
+  createUser: function (req, res){
+    //authentication
+    var testId = 250;
+    var username = req.body.username;
+
+    Users.create(testId, username)
+      .then(function (userId){
+        res.send(userId);
+      })
+      .catch(function(err){
+        console.log(err);
+        res.send(err.message);
+      })
+  }
 }
