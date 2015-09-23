e5383dbae4d3f3603fd3e23e4169e58ae274ea8f
Fangting <fangtingprahl@gmail.com>
2015-09-23 15:54:05 -0500

    client - auth - add auth Controller.js

--- client/app/auth/authController.js
+++ client/app/auth/authController.js
@@ -2,7 +2,13 @@
  angular.module('imgame.auth', [])
   .controller('AuthController', AuthController);
 
-  function AuthController($scope){
-    $scope.home = "Hello, World";
+  function AuthController($scope, $window, $location, Auth){
+
+    $scope.signin = function () {
+      Auth.signin()
+        .then(function (data) {
+          console.log("AuthController signin: ", data)
+        })
+    }
   };
 })();
