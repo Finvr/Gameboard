58a2e6c5a72e33e7e2dc78029193e15fb920f557
trsreagan3 <trsreagan3@gmail.com>
2015-09-23 10:12:48 -0500

    server-userModel-create function

---
+++ server/models/userModel.js
@@ -0,0 +1,24 @@
+var db = require ('../db.js')
+
+module.exports = {
+
+create: function (facebookId, username){
+  return db('users').insert({facebook_id:facebookId, username:username})
+  .then(function(user){
+    console.log(user);
+    return user;
+  })
+  .catch(function(err){
+    console.log(err);
+    return err;
+  })
+}
+
+
+
+
+
+
+
+
+}
