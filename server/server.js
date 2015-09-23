44054ef5d73b11c429ddf37bf662e67a3749f543
Fangting <fangtingprahl@gmail.com>
2015-09-23 16:21:56 -0500

    server - auth - add passport middleware in server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,31 @@
 var express = require('express');
 var parse = require('body-parser');
+var passport = require('passport');
+var FacebookStrategy = require("passport-facebook").Strategy;
+var config = require('./oauth.js');
 
 var app = express();
+
+passport.serializeUser(function(user, done){
+  console.log('passport serializeUser')
+  done(null, user);
+})
+
+passport.deserializeUser(function(obj, done){
+  console.log('passport deserializeUser')
+  done(null, obj);
+})
+
+passport.use(new FacebookStrategy ({
+    clientID: config.facebook.clientID,
+    clientSecret: config.facebook.clientSecret,
+    callbackURL: console.facebook.callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log("passport use FacebookStr profile: ", profile);
+    return done(err, profile)
+    }
+));
 
 app.use(parse.urlencoded({extended: true}));
 app.use(parse.json());
