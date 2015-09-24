c9df24f14fb68362cc11f190272b0701c5dd5184
Ignacio Prado <ignacio@gatogordoweb.com>
2015-09-24 17:33:25 -0500

    client - karma tests - add route / test

---
+++ test/clientTest/routingSpec.js
@@ -0,0 +1,16 @@
+describe('Routing', function() {
+  var $route;
+  beforeEach(module('imgame'));
+
+  beforeEach(inject(function($injector) {
+    $route = $injector.get('$route');
+  }));
+
+  it('Should have root ('/') route that is linked to the landing page',
+    function() {
+      //expect($route.routes['/']).to.be.ok();
+      expect($route.routes['/'].controller).toBe('HomeController');
+      expect($route.routes['/'].templateUrl).toBe(
+        'app/auth/homeTemplate.html');
+    });
+});
