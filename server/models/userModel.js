f8be03dbd91d32efab84b989ef7c7a999089aa0c
Nathan Leonard <n.leonard668@gmail.com>
2015-09-23 19:44:12 -0500

    server-userController-refactor model to take user object

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,16 +2,13 @@ var db = require ('../db.js')
 
 module.exports = {
 
-create: function (facebookId, username){
+create: function (user){
   return db('users')
-    .insert({
-      facebook_id:facebookId, 
-      username:username
-    })
-    .returning("id")
-    .then(function(user){
-      console.log(user);
-      return user;
+    .insert(user)
+    .returning("facebook_id")
+    .then(function(facebookId){
+      console.log(facebookId);
+      return facebookId;
     })
     .catch(function(err){
       console.log(err);
