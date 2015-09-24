76fd6d211a870ac62030831d65430f9f11cc10c9
Fangting <fangtingprahl@gmail.com>
2015-09-24 15:20:31 -0500

    server - route - refactor routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,35 +1,33 @@
 var userController = require('./controllers/userController.js'),
     gameController = require('./controllers/gamePostsController.js');
 
+var express = require('express');
+var router = express.Router();
 
-module.exports= function (app){
+// user routes
+router.get('/users/logout', function (req, res) {
+  req.logout();
+  res.redirect('/')
+})
 
-  //User Routes
-  app.get('/users/logout', function (req, res) {
-    req.logout();
-    res.redirect('/')
-  })
+router.get('/me', function (req, res) {
+  res.send('Authenticate the session');
+})
 
-  app.get('/me', function (req, res) {
-    res.send('Authenticate the session');
-  })
+router.get('/me/gameposts', function (req, res) {
+  gameController.getUserGamePosts(req, res);
+}) 
 
-  app.get('/me/gameposts', function (req, res) {
-    gameController.getUserGamePosts(req, res);
-  }) 
+//GamePosts Routes
+router.get('/gameposts', function (req, res) {
+  gameController.getAllGameposts(req, res);
+})
 
-  //GamePosts Routes
-  app.get('/gameposts', function (req, res) {
-    gameController.getAllGameposts(req, res);
-  })
+router.post('/gameposts', function (req, res) {
+  gameController.createGamepost(req, res);
+})
 
-  app.post('/gameposts', function (req, res) {
-    gameController.createGamepost(req, res);
-  })
+module.exports = router;
 
-
-}
-
-  
 
 
