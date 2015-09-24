8ea30490bd9a88c68e1af3e2cdbe579a6c1480db
Fangting <fangtingprahl@gmail.com>
2015-09-24 16:15:52 -0500

    server - auth - complete checkAuth func in userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,13 +9,24 @@ module.exports= {
         res.redirect('/#/create-game')
       })
       .catch(function(err){
-        console.log(err);
+        console.log("err from findOrCreateUser: ",err);
         res.send(err.message);
       })
   },
 
   checkAuth: function(req, res, next) {
-    console.log("server checkAuth req.user : ", req.user)
+    Users.find(req.user)
+      .then(function(res){
+        if (res.length !== 0) {
+          next();
+        } else {
+          res.send("User does not exist!")
+        }
+      })
+      .catch(function(err) {
+        console.log("err in checkAuth: ", err);
+        res.send("User does not exist!");
+      })
 
   }
 
