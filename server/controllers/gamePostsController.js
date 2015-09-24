23ad483ff78ae640eacc1b4b1238cbe3681ed5c3
Nathan Leonard <n.leonard668@gmail.com>
2015-09-24 09:50:09 -0500

    server-allUserPosts-move gameposts controller functions out of helpers.js and add getUserPosts function

---
+++ server/controllers/gamePostsController.js
@@ -0,0 +1,41 @@
+var GamePosts = require ('../models/gamePostsModel.js');
+
+module.exports = {
+  
+  getAllGameposts: function (req, res){
+    GamePosts.getAll()
+      .then(function (data){
+        res.send(data);
+      })
+      .catch(function(err){
+        console.log(err);
+        res.send(err.message);
+      })
+  },
+
+  createGamepost: function (req, res){
+    var gamepost = req.body;
+    
+    GamePosts.create(gamepost)
+      .then(function (data){
+        res.send(data);
+      })
+      .catch(function(err){
+        console.log(err);
+        res.send(err.message);
+      })
+  },
+
+  getUserGamePosts: function (req, res){
+    var facebookId = req.user.facebook_id;
+    GamePosts.getAll(facebookId)
+      .then(function(data){
+        res.send(data);
+      })
+      .catch(function(err){
+        console.log(err);
+        res.send(err.message);
+      })
+  }
+
+}
