92265db42db31b8eb46fdaa72f5fb40ff1a2809a
Nathan Leonard <n.leonard668@gmail.com>
2015-09-24 10:54:21 -0500

    server-userModel-build findOrCreate function

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,17 +2,33 @@ var db = require ('../db.js')
 
 module.exports = {
 
-  create: function (user){
-    return db('users')
-      .insert(user)
-      .returning("facebook_id")
-      .then(function(facebookId){
-        console.log(facebookId);
-        return facebookId;
+  findOrCreate: function (user){
+    return module.exports.fetch(user)
+      .then(function(result) {
+        if ( result.length ) {
+          console.log('result: ', result[0].facebook_id)
+          return result;
+        } else {
+          return db('users')
+            .insert(user)
+            .returning("facebook_id")
+            .then(function(facebookId){
+              console.log(facebookId[0]);
+              return facebookId[0]; //returning gives an array, but facebookId is unique
+            })
+            .catch(function(err){
+              console.log(err);
+              return err;
+            })
+        }
       })
-      .catch(function(err){
-        console.log(err);
-        return err;
-      })
-    },
+  },
+
+  fetch: function (user){
+    var facebookId = user.facebook_id;
+    return db.select()
+      .from('users')
+      .where({facebook_id: facebookId})      
+  } 
+
 }
