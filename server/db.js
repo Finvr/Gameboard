f70600a0ef624f4d93fc66a79b8ad396b2e08e5b
Fangting <fangtingprahl@gmail.com>
2015-09-25 16:22:35 -0500

    server - db - add test and production mood

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
-var config      = require('../knexfile.js');  
-var knex        = require('knex')(config);
+var config = require('../knexfile.js');  
+var env = process.env.NODE_ENV || 'development';
+var knex = require('knex')( config[env] );
 
 module.exports = knex;
 
