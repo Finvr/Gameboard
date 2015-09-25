c40d938c51f76146061128e3a1ba4755697195bc
Fangting <fangtingprahl@gmail.com>
2015-09-25 15:12:11 -0500

    server - test - set server in test file

--- test/serverTest/test.js
+++ test/serverTest/test.js
@@ -1,19 +1,7 @@
 var request = require('supertest')
   , express = require('express');
  
-var app = express();
-app.use(require('body-parser').json());
-
-app.use('/user', require('../../server/routes.js')
- 
-request(app)
-  .get('/user')
-  .expect('Content-Type', /json/)
-  // .expect('Content-Length', '20')
-  .expect(200)
-  .end(function(err, res){
-    if (err) throw err;
-  });
+var app = require('../../server/server.js');
 
 describe('GET /user', function(){
   it('respond with json', function(done){
