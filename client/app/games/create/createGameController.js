bb616266a6748b36e4adf6431eda644ee5a79546
Fangting <fangtingprahl@gmail.com>
2015-09-24 21:37:26 -0500

    client - auth - add authcheck in create game controller

--- client/app/games/create/createGameController.js
+++ client/app/games/create/createGameController.js
@@ -2,8 +2,25 @@
  angular.module('imgame.createGame', [])
  	.controller('CreateGameController', CreateGameController);
 
- 	function CreateGameController($scope, GamePost){
+ 	function CreateGameController($scope, $window, $location, GamePost, Auth){
  		$scope.game = {}
+
+    // $scope.$watch(Auth.isAuth, function(authed){
+    //     if (authed) {
+    //       $location.path('/create-game');
+    //     } else {
+    //       $location.path('/signin')
+    //     } 
+    //   });
+
+    if ($window.localStorage.userid) {
+      $location.path('/create-game');
+    } else {
+      Auth.isAuth();
+      console.log("CreateGameController window: ", $window.localStorage.userid )
+      $location.path('/')
+    } 
+
  		$scope.createGame = function(game){
  			game = { 
  				"game_location": game.location,
@@ -14,6 +31,7 @@
  			};
  			GamePost.create(game)
  				.then(function(data){
+          console.log("create gamepost: ", data)
  					console.log(data);
  				});
  		}
