248745e6ce407f73505bc6f795d08906476cf71c
Fangting <fangtingprahl@gmail.com>
2015-09-25 20:02:59 -0500

    client - Google Map - add initialize func for goodle in createGameController

--- client/app/games/create/createGameController.js
+++ client/app/games/create/createGameController.js
@@ -12,6 +12,18 @@
 
     Auth.requireAuth();
 
+    function initialize() {
+      var mapCanvas = document.getElementById('map');
+      var mapOptions = {
+        center: new google.maps.LatLng(44.5403, -78.5463),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      var map = new google.maps.Map(mapCanvas, mapOptions)
+    };
+    
+    google.maps.event.addDomListener(window, 'load', initialize);
+
  		$scope.createGame = function(game){
  			game = { 
  				"game_location": game.location,
