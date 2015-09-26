8f9181e04dc3557fed7ff1842e6fc7b60c19ef0e
Fangting <fangtingprahl@gmail.com>
2015-09-26 11:34:18 -0500

    client - create game - date time format

--- client/app/games/create/datetime.js
+++ client/app/games/create/datetime.js
@@ -1,31 +1,31 @@
 (function(){
 
   angular.module('imgame.datetime', ['scDateTime'])
-  .value('scDateTimeConfig', {
-    defaultTheme: 'material',
-    autosave: false,
-    defaultMode: 'date',
-    defaultDate: undefined, //should be date object!!
-    displayMode: undefined,
-    defaultOrientation: false,
-    displayTwentyfour: false,
-    compact: false
-})
-    // .value('scDateTimeI18n', {
-    //   previousMonth: "Previous Month",
-    //   nextMonth: "Next Month",
-    //   incrementHours: "Increment Hours",
-    //   decrementHours: "Decrement Hours",
-    //   incrementMinutes: "Increment Minutes",
-    //   decrementMinutes: "Decrement Minutes",
-    //   switchAmPm: "Switch AM/PM",
-    //   now: "Now",
-    //   cancel: "Cancel",
-    //   save: "Save",
-    //   weekdays: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-    //   switchTo: 'Switch to',
-    //   clock: 'Clock',
-    //   calendar: 'Calendar'
-    // });
+//   .value('scDateTimeConfig', {
+//     defaultTheme: 'material',
+//     autosave: false,
+//     defaultMode: 'date',
+//     defaultDate: undefined, //should be date object!!
+//     displayMode: undefined,
+//     defaultOrientation: false,
+//     displayTwentyfour: false,
+//     compact: false
+// })
+    .value('scDateTimeI18n', {
+      previousMonth: "Previous Month",
+      nextMonth: "Next Month",
+      incrementHours: "Increment Hours",
+      decrementHours: "Decrement Hours",
+      incrementMinutes: "Increment Minutes",
+      decrementMinutes: "Decrement Minutes",
+      switchAmPm: "Switch AM/PM",
+      now: "Now",
+      cancel: "Cancel",
+      save: "Save",
+      weekdays: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+      switchTo: 'Switch to',
+      clock: 'Clock',
+      calendar: 'Calendar'
+    });
     
 })();
