28bd0960e09755ef35798b15c4a830ac4207ac44
Fangting <fangtingprahl@gmail.com>
2015-09-28 15:58:38 -0500

    server - test - add clear db before each test

--- server/db.js
+++ server/db.js
@@ -2,6 +2,29 @@ var config = require('../knexfile.js');
 var env = process.env.NODE_ENV || 'development';
 var knex = require('knex')( config[env] );
 
+if (process.env.NODE_ENV  === 'test') {
+
+  knex.deleteEverything = function () {
+    return knex.table('users_games').truncate()
+      .then(function(){
+        return knex.table('users_games').select('*');
+      })
+      .then(function(ug){
+        return knex.table('requests').truncate();
+      })
+      .then(function(){
+        return knex.table("gameposts").delete();
+      })
+      .then(function(){
+        return knex.table('users').delete();
+      })
+      .catch(function(err){
+        console.log("db.js knex users ERROR: ", err)
+      })
+  }
+
+}
+
 module.exports = knex;
 
 // if (process.env.NODE_ENV != 'production') {
