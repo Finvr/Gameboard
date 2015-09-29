b7673ab503719a85455f5f9239a6d28795448778
Fangting <fangtingprahl@gmail.com>
2015-09-29 14:05:07 -0500

    server-request-create model func minor fix

--- server/models/requestsModel.js
+++ server/models/requestsModel.js
@@ -54,27 +54,28 @@ module.exports = {
   },
 
   create: function(request) {
-      return db('requests')
-        .select()
-        .where({
-          user_id: request.user_id,
-          gamepost_id: request.gamepost_id
-        })
-        .then(function(request){
-          if (request.length > 0) {
-            return "Request has already been submmited once!";
-          } else {
-            return db('requests')
-              .insert(request)
-              .returning("id")
-              .then(function(requestId){
-                return module.exports.find(requestId[0]);
-              })
-              .then(function(request){
-                return request[0];
-              });
-          }
-        })
+    return db('requests')
+      .select()
+      .where({
+        user_id: request.user_id,
+        gamepost_id: request.gamepost_id
+      })
+      .then(function(requests){
+        if (requests.length > 0) {
+          return "Request has already been submmited once!";
+        } else {
+          return db('requests')
+            .insert(request)
+            .returning("id")
+            .then(function(requestId){
+              console.log("create request with requestId: ", requestId);
+              return module.exports.find(requestId[0]);
+            })
+            .then(function(request){
+              return request[0];
+            });
+        }
+      })
   },
 
   deleteRequest: function(request){
