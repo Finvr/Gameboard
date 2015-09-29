046ed3519278a69fbdc7bf8bf0e6091f7bce3e20
Fangting <fangtingprahl@gmail.com>
2015-09-28 19:27:36 -0500

    client - request - add request func in service

--- client/app/services/serviceBrowseGames.js
+++ client/app/services/serviceBrowseGames.js
@@ -5,17 +5,29 @@
   function BrowseGames($http){
 		function getGames(){
 			return $http({
-		        method: 'GET',
-		        url: '/gameposts'
-		      })
-		      .then(function(resp) {
-		      	console.log("resp",resp.data);
-		        return resp.data;
-		      });
-		 };
+        method: 'GET',
+        url: '/gameposts'
+      })
+      .then(function(resp) {
+      	console.log("resp",resp.data);
+        return resp.data;
+      });
+		};
+
+		function sendRequest(message, gamepostsId){
+			return $http({
+				method: 'POST',
+				url: '/gameposts/' + gamepostsId + "/requests",
+				data: message
+			})
+			.then(function(resp){
+				console.log("resp in sendRequest: ", resp)
+			})
+		}
 		  	
-		  	return {
-		  		getGames: getGames
-		  	};
+  	return {
+  		getGames: getGames,
+  		sendRequest: sendRequest
+  	};
  		};
 })();
