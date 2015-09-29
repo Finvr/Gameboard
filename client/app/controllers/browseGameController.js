b35fafd8585df2912d68f71287b812d7b4f0a7d4
Fangting <fangtingprahl@gmail.com>
2015-09-28 19:27:04 -0500

    client-request-add sendRequest func in controller

--- client/app/controllers/browseGameController.js
+++ client/app/controllers/browseGameController.js
@@ -6,9 +6,18 @@
 
 		$scope.home = "djsaldjaskl";
 		$scope.games = [];
+		$scope.requestMessage = {comments: ''};
+
 		BrowseGames.getGames().then(function(resp) {
 			console.log("inside getGsmes", resp);
 			$scope.games = resp;
 		});
+
+		$scope.sendRequest = function(game) {
+			console.log("sendRequest games: ", $scope.games)
+			console.log("sendRequest game: ", game)
+			console.log("sendRequest: request ", $scope.requestMessage)
+			BrowseGames.sendRequest()
+		}
 	}
 })();
