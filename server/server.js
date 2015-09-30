5a78f30e5cbc54541b304fa9f6e4a845550182bc
Fangting <fangtingprahl@gmail.com>
2015-09-30 17:36:39 -0500

    deploy - deploye mvp

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ var express           = require('express'),
     Utils             = require('./utils/helpers.js'),
     passport          = require('passport'),
     FacebookStrategy  = require("passport-facebook").Strategy,
-    // for deploy:
-    //config            = require('./oauth.js'),
+    // for deploy, please comments the following line
+    config            = require('./oauth.js'),
     sessions          = require('cookie-session'),
     //logger            = require('morgan'),
     router            = require('./routes.js'),  
