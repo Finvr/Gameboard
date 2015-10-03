1d3fae5f3b1fa4a9e3c8468436d03b9a4edf7a1f
Nathan Leonard <n.leonard668@gmail.com>
2015-10-03 12:08:36 -0500

    server-userModel-update access token and username on each user login

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,12 +3,15 @@ var db = require ('../db.js')
 module.exports = {
 
   findOrCreate: function(user) {
+    var facebookToken = user.facebook_token;
+    var username = user.username;
+
     return db.select()
       .from('users')
       .where({facebook_id: user.facebook_id})
       .then(function(result) {
         if ( result.length ) {
-          return result[0];
+          return updateUser(result[0], facebookToken, username);
         } else {
           return create(user);
         }
@@ -42,5 +45,25 @@ function create(user) {
         .then(function(user){
           return user[0]; //returning gives an array, but id is unique
         })
+    })
+};
+
+function updateUser(user, facebookToken, username) {
+  return db('users')
+    .where({id: user.id})
+    .update({
+      facebook_token: facebookToken,
+      username: username,
+      updated_at: db.raw('now()')
+    })
+    .returning('id')
+    .then(function(userId) {
+      return module.exports.find(userId[0])
+    })
+    .then(function(user) {
+      return user[0]
+    })
+    .catch(function(err) {
+      return err;
     })
 };
