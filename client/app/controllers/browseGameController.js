233020a37f04a8281076e8edf62aa5882b020e5b
Fangting <fangtingprahl@gmail.com>
2015-10-05 16:23:40 -0500

    client - browse game - create diatance formular on lat and lng

--- client/app/controllers/browseGameController.js
+++ client/app/controllers/browseGameController.js
@@ -53,6 +53,20 @@
 				$scope.games = resp;
 			});
 
+		function distance(lat1, lon1, lat2, lon2) {
+			var radlat1 = Math.PI * lat1/180
+			var radlat2 = Math.PI * lat2/180
+			var radlon1 = Math.PI * lon1/180
+			var radlon2 = Math.PI * lon2/180
+			var theta = lon1-lon2
+			var radtheta = Math.PI * theta/180
+			var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+			dist = Math.acos(dist)
+			dist = dist * 180/Math.PI
+			dist = dist * 60 * 1.1515
+			return Math.round(dist*100)/100;
+		}
+
 		$scope.dateFilter = function(gameTime) {
 			gameTime = Date.parse(gameTime);
 			startTime = Date.parse($scope.startDateFilter);
