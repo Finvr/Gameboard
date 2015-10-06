05278666b509f27315b2a3aaf28d97a7188d62b9
Ignacio Prado <ignacio@gatogordoweb.com>
2015-10-06 15:25:57 -0500

    client - v. 1.0 features - add updateProfile method to ProfileController

--- client/app/controllers/profileController.js
+++ client/app/controllers/profileController.js
@@ -4,17 +4,28 @@ angular.module('imgame.profile', [])
 
 	function ProfileController($scope, Profile){
     var getMyProfile = function(){
-      Profile.getProfile().then(function(profile){
-        $scope.myProfile = profile;
-        console.log($scope.myProfile);
-      });
+      Profile.getProfile()
+      	.then(function(profile){
+        	$scope.myProfile = profile;
+        	console.log($scope.myProfile);
+      	});
     };
 
     $scope.updateProfile = function(profileData){
-    	
+    	profileData = {
+    		id: 1,
+    		location: "Miami Shores, FL",
+    		about_me: "I'm the greatest at this",
+    		games_list: "Stratego, Dominos, Risk, Canasta"
+    	}
+    	Profile.updateProfile(profileData)
+    		.then(function(data){
+    			console.log(data);
+    		});
     };
 
     getMyProfile();
+    //$scope.updateProfile({});
 	};
 
 })();
