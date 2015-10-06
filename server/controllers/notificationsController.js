009840d951f6a6bc4581c57ab59a5adfcd497741
Nathan Leonard <n.leonard668@gmail.com>
2015-10-06 15:03:24 -0500

    server-notifications-create updateNotifications function

--- server/controllers/notificationsController.js
+++ server/controllers/notificationsController.js
@@ -11,5 +11,15 @@ module.exports = {
       .catch(function (err) {
         res.status(500).send(err.message);
       });
+  },
+
+  updateNotifications: function (req, res) {
+    var updated = req.body;
+    //Figure out how to do this for-loop in async
+    for ( var i = 0; i < updated.length; i++ ) {
+      Notifications.update(updated[i])
+    }
+    res.send(200);
   }
+  
 };
