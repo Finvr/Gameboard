2d4cd0896c27cf66acd742961424e7eb58db4d38
Ignacio Prado <ignacio@gatogordoweb.com>
2015-10-06 19:49:52 -0500

    client - karma tests - add .init() tests to myGamesControllerSpec

--- test/clientTest/myGamesCtrlSpec.js
+++ test/clientTest/myGamesCtrlSpec.js
@@ -1,13 +1,33 @@
 describe('MyGamesController', function() {
   beforeEach(module('imgame'));
 
-  var $controller;
+  var ctrl, scope;
 
-  beforeEach(inject(function(_$controller_){
-    $controller = _$controller_;
+  beforeEach(inject(function($controller, $rootScope){
+  	scope = $rootScope.$new();
+  	ctrl = $controller('MyGamesController', {
+  		$scope: scope
+  	}); 
   }));
 
-  it("should understand truth", function(){
-    expect(true).toBe(true);
+  /*
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));*/
+
+  describe("$scope.init", function(){
+    it("should be defined", function(){
+    	expect(scope.init).toBeDefined();
+    });
+    it("should initialize gameToShowDetails to null", function(){
+    	expect(scope.gameToShowDetails).toBeNull();
+    });
+    it("should initialize gameToCancel to null", function(){
+    	expect(scope.gameToCancel).toBeNull();
+    });
+    it("should initialize gameToApprove to null", function(){
+    	expect(scope.gameToApprove).toBeNull();
+    });
+    
   });
 });
