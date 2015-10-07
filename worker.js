9323e4565aa7cbb608b2fd30500d29a767e34546
Nathan Leonard <n.leonard668@gmail.com>
2015-10-07 17:34:08 -0500

    server-notifications-create notification for host of ended game

--- worker.js
+++ worker.js
@@ -6,15 +6,31 @@ var GamePosts = require('./server/models/gamePostsModel.js'),
 
 setInterval(function() {
   var reqIds = [];
+  var gameIds;
+
   GamePosts.checkForExpired()
   .then(function (expiredIds) {
     console.log("Expired Gameposts: ", expiredIds);
+    gameIds = expiredIds;
     return helpers.promiseFor(function (i) {
       return i < expiredIds.length;
     }, function (i) {
       return Requests.declineAll(expiredIds[i])
         .then(function (requestIds) {
           reqIds = reqIds.concat(requestIds);
+          return ++i;
+        });
+    }, 0)
+  })
+  .then(function() {
+    return helpers.promiseFor(function (i) {
+      return i < gameIds.length;
+    }, function (i) {
+      return GamePosts.fetchById(gameIds[i])
+        .then(function (gamepost) {
+          return Notes.create(gamepost.host_id, "game ended", gamepost.id);
+        })
+        .then(function () {
           return ++i;
         });
     }, 0)
@@ -26,7 +42,7 @@ setInterval(function() {
     }, function (i) {
       return Requests.fetchById(reqIds[i])
         .then(function (request) {
-          return Notes.create(request.user_id, "game ended", request.id);
+          return Notes.create(request.user_id, "game ended", request.gamepost_id);
         })
         .then(function () {
           return ++i;
