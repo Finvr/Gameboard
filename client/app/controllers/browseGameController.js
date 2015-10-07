efcd9bd2a2e1d8db50ecee242e268a2bc18a1699
Fangting <fangtingprahl@gmail.com>
2015-10-07 17:26:53 -0500

    client - browseGame - date filter add a day to endDate

--- client/app/controllers/browseGameController.js
+++ client/app/controllers/browseGameController.js
@@ -108,9 +108,9 @@
 		$scope.dateFilter = function(gameTime) {
 			var gameTime = Date.parse(gameTime);
 			var startTime = Date.parse($scope.startDateFilter);
-			var endTime = Date.parse($scope.endDateFilter);
-			if (!$scope.startDateFilter || gameTime > startTime) {
-				if (!$scope.endDateFilter || gameTime < endTime) {
+			var endTime = Date.parse($scope.endDateFilter) +  1000 * 60 * 60 * 24;
+			if (!$scope.startDateFilter || gameTime > startTime - 1) {
+				if (!$scope.endDateFilter || gameTime < endTime + 1) {
 					return true;				
 				}
 			} else {
