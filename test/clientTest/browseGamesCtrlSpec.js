7143c095d7c989f5e0cd3ba2b56e30e649200746
Ignacio Prado <ignacio@gatogordoweb.com>
2015-10-07 16:40:02 -0500

    client - karma tests - add scaffolding for browseGamesSpec

--- test/clientTest/browseGamesCtrlSpec.js
+++ test/clientTest/browseGamesCtrlSpec.js
@@ -1,12 +1,19 @@
-describe('BrowseGamesController', function() {
+describe('BrowseGameController', function() {
   beforeEach(module('imgame'));
 
-  var $controller;
+  var ctrl, scope; 
 
-  beforeEach(inject(function(_$controller_){
-    $controller = _$controller_;
+  beforeEach(inject(function($controller, $rootScope){
+   	scope = $rootScope.$new();
+  	ctrl = $controller('BrowseGameController', {
+  		$scope: scope
+  	});
   }));
 
+
   describe('$scope.disFilter', function(){
+  	it("should be defined", function(){
+  		expect(scope.disFilter).toBeDefined();
+  	});
   });
 });
