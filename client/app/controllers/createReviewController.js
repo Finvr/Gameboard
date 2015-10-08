4ded929fc4e5d3a034f71b8d601735509826050e
trsreagan3 <trsreagan3@gmail.com>
2015-10-08 15:09:22 -0500

    working on review model

--- client/app/controllers/createReviewController.js
+++ client/app/controllers/createReviewController.js
@@ -1,3 +1,22 @@
 (function(){
-	
+	angular.module('imgame.createReview', [])
+	.controller('CreateReviewController', CreateReviewController);
+
+	function CreateReviewController($scope, $window, Auth, Review){
+		$scope.review = {};
+
+		$scope.createReview = function (review){
+			review = {
+				"showed_up": $scope.review.showed_up,
+				"rating": $scope.review.rating,
+				"reviewee_id": $scope.review.reviewee_id,
+				"reviewer_id": $scope.review.reviewer_id,
+				"gamepost_id": $scope.review.gamepost_id
+			}
+			console.log("create review: ", $scope.review)
+		}
+	}
+	return {
+    createReview: createReview
+  } 
 })
