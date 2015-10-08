60927f961a53fc7de5dca09c5e6d206dbafbae83
Nathan Leonard <n.leonard668@gmail.com>
2015-10-07 19:02:16 -0500

    server-notifications-refactor worker file to clean up notifications code

--- worker.js
+++ worker.js
@@ -3,53 +3,66 @@ var GamePosts = require('./server/models/gamePostsModel.js'),
     helpers   = require('./server/utils/helpers.js'),
     Requests  = require('./server/models/requestsModel.js');
 
-
-setInterval(function() {
+function findExpired () {
   var reqIds = [];
   var gameIds;
 
-  GamePosts.checkForExpired()
-  .then(function (expiredIds) {
-    console.log("Expired Gameposts: ", expiredIds);
-    gameIds = expiredIds;
-    return helpers.promiseFor(function (i) {
-      return i < expiredIds.length;
-    }, function (i) {
-      return Requests.declineAll(expiredIds[i])
-        .then(function (requestIds) {
-          reqIds = reqIds.concat(requestIds);
-          return ++i;
-        });
-    }, 0)
-  })
-  .then(function() {
-    return helpers.promiseFor(function (i) {
-      return i < gameIds.length;
-    }, function (i) {
-      return GamePosts.fetchById(gameIds[i])
-        .then(function (gamepost) {
-          return Notes.create(gamepost.host_id, "game ended", gamepost.id);
-        })
-        .then(function () {
-          return ++i;
-        });
-    }, 0)
-  })
-  .then(function () {
-    console.log("IDs array:", reqIds);
-    return helpers.promiseFor(function (i) {
-      return i < reqIds.length;
-    }, function (i) {
-      return Requests.fetchById(reqIds[i])
-        .then(function (request) {
-          return Notes.create(request.user_id, "game ended", request.gamepost_id);
-        })
-        .then(function () {
-          return ++i;
-        });
-    }, 0)
-  })
-  .then(function () {
-    console.log("All done!");
-  });
+  return GamePosts.checkForExpired()
+    .then(function (expiredIds) {
+      console.log("Expired Gameposts: ", expiredIds);
+      gameIds = expiredIds;
+      return helpers.promiseFor(function (i) {
+        return i < expiredIds.length;
+      }, function (i) {
+        return Requests.declineAll(expiredIds[i])
+          .then(function (requestIds) {
+            reqIds = reqIds.concat(requestIds);
+            return ++i;
+          });
+      }, 0)
+    })
+    .then(function () {
+      return [reqIds, gameIds];
+    })
+};
+
+function sendHostNotifications (reqIds, gameIds) {
+  return helpers.promiseFor(function (i) {
+    return i < gameIds.length;
+  }, function (i) {
+    return GamePosts.fetchById(gameIds[i])
+      .then(function (gamepost) {
+        return Notes.create(gamepost.host_id, "game ended", gamepost.id);
+      })
+      .then(function () {
+        return ++i;
+      });
+  }, 0)
+    .then(function () {
+      return reqIds;
+    })
+};
+
+function sendUserNotifications (reqIds) {
+  return helpers.promiseFor(function (i) {
+    return i < reqIds.length;
+  }, function (i) {
+    return Requests.fetchById(reqIds[i])
+      .then(function (request) {
+        return Notes.create(request.user_id, "game ended", request.gamepost_id);
+      })
+      .then(function () {
+        return ++i;
+      });
+  }, 0)
+};
+
+setInterval(function () {
+  findExpired()
+    .then(function (idArrays) {
+      return sendHostNotifications (idArrays[0], idArrays[1]);
+    })
+    .then(function (reqIds) {
+      return sendUserNotifications(reqIds);
+    })
 }, 5000);
