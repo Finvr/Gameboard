55f3f353f0a48fb25278dc90ec272d8c714713b0
Ignacio Prado <ignacio@gatogordoweb.com>
2015-10-09 15:10:09 -0500

    client - v. 1.0 features - refactor Users.all to return data

--- client/app/services/serviceUsers.js
+++ client/app/services/serviceUsers.js
@@ -1,16 +1,15 @@
 angular
 	.module("imgame.service")
 	.factory("Users", function UsersFactory($http){
-		var list = [];
 		var all = function(){
-			return $http.get({
+			return $http({
+				method: "GET",
 				url: "/users"
-			}).then(function(data){
-				list = data;
+			}).then(function(resp){
+				return resp.data;
 			});
-		}
+		};
 		return {
 			all: all
-			list: list
 		};
 	});
