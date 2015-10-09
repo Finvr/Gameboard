3e2a542f6d7b5ad440e983a5683bb77ef4efa15c
Fangting <fangtingprahl@gmail.com>
2015-10-09 17:43:27 -0500

    client - undisplay games after reviewed

--- client/app/controllers/profileController.js
+++ client/app/controllers/profileController.js
@@ -4,92 +4,100 @@ angular.module('imgame.profile', [])
 
 	function ProfileController($scope, Profile, Auth, $route, $location, Review){
 
-        Auth.requireAuth();
+    Auth.requireAuth();
 
-        var getProfile = function(){
-          Profile.getProfile($route.current.params.id)
-          	.then(function(profile){
-            	$scope.myProfile = profile;
-            	$scope.savedProfile = $scope.myProfile;
-          	});
-        };
+    var getProfile = function(){
+      Profile.getProfile($route.current.params.id)
+      	.then(function(profile){
+        	$scope.myProfile = profile;
+        	$scope.savedProfile = $scope.myProfile;
+      	});
+    };
 
-        $scope.getRecentGames = function(){
-            Profile.getRecentGames()
-                .then(function(games){
-                    console.log("getRecentGames in Profile: ", games);
-                    $scope.recentGames = games;
-                })
-        };
-
-        $scope.close = function(selector) {
-            $(selector).closeModal();
-        };
-
-        $scope.updateProfile = function(profileData){
-            Profile.updateProfile(profileData)
-                .then(function(data){
-                    $scope.update = null;
-        			init();
-                });
-        };
-
-        $scope.showInput = function(selector){
-            if (!$scope.myProfile || !$scope.myProfile.viewId) {
-                $scope.update = true;
-                $(selector).find('.read').hide();
-                $(selector).find('.write').show();            
-            }
-        };
-
-        $scope.openRateModal = function(game) {
-            if (game.host_name) {
-                game.playerPics.unshift({picture: game.host_pic,
-                    user_id: game.host_id,
-                    username: game.host_name
-                });
-            }
-            game.playerPics.forEach(function(player){
-                player.skip = true;
-                player.showed_up
-                player.rating = 3;
+    $scope.getRecentGames = function(){
+      Profile.getRecentGames()
+        .then(function(games){
+          games = games.filter(function(game){
+            var result = true;
+            $scope.existingReviews.forEach(function(review){
+              if (review.gameposts_id === game.gamepost_id && review.reviewer_id === game.user_id) {
+                result = false;
+              }
             })
-            $scope.currentRateGame = game;
-        };
+            return result;
+          })                     
+          $scope.recentGames = games;
+        })
+    };
 
-        $scope.getReviews = function() {
-            Profile.getReviews()
-                .then(function(data){
-                    console.log("getReviews controller: ", data)
-                })
-        };
+    $scope.close = function(selector) {
+      $(selector).closeModal();
+    };
 
-        var init = function(){
-        	$('.profile-field').find('span.write').hide();
-        	$('.profile-field').find('span.read').show();
-        	getProfile();
-            $scope.getRecentGames();
-            $scope.getReviews();
+    $scope.updateProfile = function(profileData){
+      Profile.updateProfile(profileData)
+        .then(function(data){
+          $scope.update = null;
+			    init();
+        });
+    };
+
+    $scope.showInput = function(selector){
+      if (!$scope.myProfile || !$scope.myProfile.viewId) {
+        $scope.update = true;
+        $(selector).find('.read').hide();
+        $(selector).find('.write').show();            
+      }
+    };
+
+    $scope.openRateModal = function(game) {
+      if (game.host_name) {
+        game.playerPics.unshift({picture: game.host_pic,
+          user_id: game.host_id,
+          username: game.host_name
+        });
+      }
+      game.playerPics.forEach(function(player){
+        player.skip = true;
+        player.showed_up
+        player.rating = 3;
+      })
+      $scope.currentRateGame = game;
+    };
+
+    $scope.getReviews = function() {
+      Profile.getReviews()
+        .then(function(data){
+          $scope.existingReviews = data;
+        })
+    };
+
+    var init = function(){
+    	$('.profile-field').find('span.write').hide();
+    	$('.profile-field').find('span.read').show();
+    	getProfile();
+      $scope.getRecentGames();
+      $scope.getReviews();
+    }
+
+    $scope.sendReviews = function (players){
+      var gamepostId = $scope.currentRateGame.gamepost_id;
+      var reviews = [];
+      for (var i = 0; i < players.length; i++){
+        if (!players[i].skip) {
+          players[i].reviewee_id = players[i].user_id
+          players[i].gameposts_id = gamepostId;
+          delete players[i].skip;
+          reviews.push(players[i])
         }
-
-        $scope.sendReviews = function (players){
-            var gamepostId = $scope.currentRateGame.gamepost_id;
-            var reviews = [];
-            for (var i = 0; i < players.length; i++){
-                if (!players[i].skip) {
-                    players[i].reviewee_id = players[i].user_id
-                    players[i].gameposts_id = gamepostId;
-                    delete players[i].skip;
-                    reviews.push(players[i])
-                }
-            }
-            Review.createReview(reviews)
-                .then(function(reviews){
-                    console.log("reviews from profile controller : ", reviews)                
-                });
-        }
-        
-        init();
+      }
+      Review.createReview(reviews)
+        .then(function(reviews){
+          console.log("reviews from profile controller : ", reviews)                
+        });
+    }
+    
+    init();
 	};
 
 })();
