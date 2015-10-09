9b73bb25a35e309925e3db977762494718f92824
Ignacio Prado <ignacio@gatogordoweb.com>
2015-10-09 14:42:05 -0500

    client - v. 1.0 features - create list variable in Users and have User.all populate it with data

--- client/app/services/serviceUsers.js
+++ client/app/services/serviceUsers.js
@@ -1,12 +1,16 @@
 angular
 	.module("imgame.service")
 	.factory("Users", function UsersFactory($http){
+		var list = [];
 		var all = function(){
 			return $http.get({
 				url: "/users"
+			}).then(function(data){
+				list = data;
 			});
 		}
 		return {
 			all: all
-		}
+			list: list
+		};
 	});
