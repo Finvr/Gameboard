410b581a95fc329008aca512d328aa66ac3da499
Ignacio Prado <ignacio@gatogordoweb.com>
2015-10-09 12:30:30 -0500

    client - karma test - test that User.all makes correct http request

--- test/clientTest/usersServiceSpec.js
+++ test/clientTest/usersServiceSpec.js
@@ -1,6 +1,6 @@
 describe("Users service", function(){
 
-	var Users, http; 
+	var Users, http, users; 
 
 	beforeEach(module('imgame'));
 
@@ -8,12 +8,28 @@ describe("Users service", function(){
 	beforeEach(inject(function(_Users_, $httpBackend){
 		Users = _Users_;
 		http = $httpBackend;
+		users = [{
+			id: 1,
+			name: "Ignacio Prado",
+			picture: "http://smile.jpg"
+		}, {
+			id: 2,
+			name: "Nathan Leonard",
+			picture: "http://beauty.jpg"
+		}]
 	}));
-	it("should understand truth", function(){
-		expect(true).toBe(true);
-	});
 
 	it("should be defined", function(){
 		expect(Users).toBeDefined;
+	});
+
+	describe("Users.all", function(){
+		it("should be defined", function(){
+			expect(Users.all).toBeDefined();
+		});
+		it("should send a get /users request when called", function(){
+			Users.all();
+			http.expectGET('/users').respond(200);
+		});
 	});
 });
