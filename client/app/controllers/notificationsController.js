70192a7cde4c18b56dd7c0b7faadc528be47de45
Vaishnavi Reddy <vaireddy11@gmail.com>
2015-10-09 12:00:14 -0500

    client refactor code notifications

--- client/app/controllers/notificationsController.js
+++ client/app/controllers/notificationsController.js
@@ -15,13 +15,13 @@ function NotificationsController($scope, Auth, $route, $location){
 		var hours = Math.floor((mins) / (60));
 		var days = Math.floor((hours) / (24));
 		if (days >= 1) {
-			return days + " days ago";
+			return (days === 1) ? days + " day ago" : days + " days ago";
 		}
 		else if (hours >= 1) {
-			return hours + " hours ago";
+			return (hours === 1) ? hours + " hour ago" : hours + " hours ago";
 		}
 		else if (mins >= 1) {
-			return mins + " mins ago";
+			return (mins === 1) ? mins + " min ago" : mins + " mins ago";
 		}
 		else return seconds + " seconds ago";
 	}
