a53e66588866d7e50cc495bb4086ab5a79728834
Fangting <fangtingprahl@gmail.com>
2015-10-09 16:09:11 -0500

    server - review - create review controller func

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -1,4 +1,5 @@
 var Reviews = require('../models/reviewsModel.js');
+var helpers  = require ('../utils/helpers.js');
 
 module.exports = {
   getReviewsByReviewerId: function(req, res) {
@@ -7,5 +8,26 @@ module.exports = {
       .then(function(reviews){
         res.send(reviews);
       })
+  },
+
+  createReviewsByGamePostsId: function(req, res) {
+    var reviewerId = req.user.id;
+    var reviews = req.body;
+    return helpers.promiseFor(
+      function(i){
+        return i < req.body.length;
+      },
+      function(i) {
+        reviews[i].reviewer_id = reviewerId;
+        return Reviews.findOrCreateReview(reviews[i])
+          .then(function(){
+            i ++;
+            return i;            
+          })
+      }, 0)
+    .then(function(counts){
+      console.log("Created ", counts, " reviews for gamepost ", reviews[0].gameposts_id);
+      res.sendStatus(200);
+    });
   }
 }
