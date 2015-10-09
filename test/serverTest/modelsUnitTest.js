75c49b11fd2340b6eece250ece2700ef962c34b9
Fangting <fangtingprahl@gmail.com>
2015-10-08 19:54:23 -0500

    server - testing - add 4 requestModel function unit testing

--- test/serverTest/modelsUnitTest.js
+++ test/serverTest/modelsUnitTest.js
@@ -4,6 +4,7 @@ var request = require('supertest');
 var expect = require('chai').expect;
 var userModel = require('../../server/models/userModel.js');
 var gamePostsModel = require('../../server/models/gamePostsModel.js');
+var requestsModel = require('../../server/models/requestsModel.js');
 var db = require('../../server/db.js')
  
 var app = require('../../server/server.js');
@@ -44,6 +45,7 @@ describe('userModel', function(){
         done();
       })
       .catch(function(err){
+        throw err;
         console.log("findOrCreate 1 error: ", err.message);
         done();
       })
@@ -56,6 +58,7 @@ describe('userModel', function(){
         done();
       })
       .catch(function(err){
+        throw err;
         console.log("findOrCreate 2 error: ", err.message);
         done();
       })
@@ -76,7 +79,8 @@ describe('userModel', function(){
         expect(result.facebook_id).to.equal('1234');   
         done(); 
       })
-      .catch(function(err) {    
+      .catch(function(err) {   
+        throw err; 
         console.log("findOrCreate update error: ", err.message);  
         done();  
       })
@@ -88,10 +92,11 @@ describe('userModel', function(){
         return userModel.getToken(user.id)
       })
       .then(function(token) {
-        expect(token).to.equal('abc');
+        expect(token.facebook_token).to.equal('abc');
         done();
       })
       .catch(function(err) {
+        throw err;         
         console.log("getToken error: ", err.message)
         done();
       })
@@ -112,6 +117,7 @@ describe('userModel', function(){
         done();
       })
       .catch(function(err){
+        throw err;         
         console.log("Error: ", err);
         done();
       })
@@ -159,6 +165,7 @@ describe('gamePostsModel', function(){
         };
       })
       .catch(function(err){
+        throw err;         
         console.log("Error: ", err);
       })
   });
@@ -178,6 +185,7 @@ describe('gamePostsModel', function(){
         done();
       })
       .catch(function(err){
+        throw err; 
         console.log("gamePost create error: ", err.message);
         done();
       })
@@ -193,6 +201,7 @@ describe('gamePostsModel', function(){
         done();
       })
       .catch(function(err){
+        throw err;
         console.log("getAll error: ", err.message);
         done();
       })
@@ -206,6 +215,7 @@ describe('gamePostsModel', function(){
         done();
       })
       .catch(function(err){
+        throw err;
         console.log("getAll by userId error: ", err.message);
         done();
       })
@@ -224,7 +234,148 @@ describe('gamePostsModel', function(){
         done();
       })
       .catch(function(err){
+        throw err;
         console.log("Error: ", err)
+        done();
+      })
+  });
+
+});
+
+describe('requestsModel', function(){
+
+  var user1 = {
+    username: 'never',
+    facebook_id: '1234'
+  }
+
+  var gamepost1 = { 
+    game_location: '2213 Santa Maria St, Austin, TX 78702, USA',
+    game: 'Clue',
+    player_count: 20,
+    game_datetime: '2015-10-12T02:00:00.000Z',
+  };
+
+  var request1 = {
+    status: "pending",
+    comments: "Can i join?"
+  }
+
+  var user2 = {
+    username: 'again',
+    facebook_id: '5678'
+  };
+
+  var gamepost2 = { 
+    game_location: '123 Avenue Q, New York, NY 12345, USA',
+    game: 'Chess',
+    player_count: 2,
+    game_datetime: '2015-10-11T02:00:00.000Z',
+  };
+
+  var request2 = {
+    status: "pending",
+    comments: "Please, let me join."
+  }
+
+  before(function() {
+    return userModel.findOrCreate(user1)     
+      .then(function(user){
+        gamepost1.host_id = user.id;
+        request1.user_id = user.id;
+        user1.id = user.id;
+        return gamePostsModel.create(gamepost1);
+      })
+      .then(function(gamepostId){
+        gamepost1.id = gamepostId;
+        request2.gamepost_id = gamepostId;
+        return gamepostId;
+      })
+      .then(function(id) {
+        return userModel.findOrCreate(user2);
+      })      
+      .then(function(user){
+        user2.id = user.id;
+        gamepost2.host_id = user.id;
+        request2.user_id = user.id;
+        return gamePostsModel.create(gamepost2);
+      })
+      .then(function(gamepostId){
+        gamepost2.id = gamepostId;
+        request1.gamepost_id = gamepostId;
+        return gamepostId;
+      })      
+      .catch(function(err){
+        throw err;
+        console.log("Error: ", err);
+      })
+  });
+
+
+  it('create should create a request in the db', function(done){
+    requestsModel.create(request1)
+      .then(function(request){
+        request1.id = request.id;
+        expect(request.id);
+        expect(request.comments).to.equal('Can i join?');
+        expect(request.status).to.equal('pending')
+        done();
+      })
+      .catch(function(err){
+        throw err;
+        console.log("request create error: ", err.message);
+        done();
+      })
+  });
+
+  it('find function should find an existing request from database ', function(done){
+    requestsModel.find(request1.id)
+      .then(function(request){
+        expect(request.length).to.equal(1);
+        request = request[0];
+        expect(request.gamepost_id).to.equal(request1.gamepost_id);
+        expect(request.user_id).to.equal(request1.user_id);
+        expect(request.comments).to.equal('Can i join?');
+        expect(request.status).to.equal('pending');
+        done();
+      })
+      .catch(function(err){
+        throw err;
+        console.log("find request error: ", err.message);
+        done();
+      })
+  });
+
+  it('getRequestByGameId function should return all requests by gameId', function(done){
+    requestsModel.getRequestByGameId(gamepost2.id)
+      .then(function(request){
+        expect(request.length).to.equal(1);
+        request = request[0];
+        expect(request.user_id).to.equal(request1.user_id);
+        expect(request.comments).to.equal('Can i join?');
+        expect(request.status).to.equal('pending');
+        done();
+      })
+      .catch(function(err){
+        throw err;
+        console.log("getRequestByGameId error: ", err.message);
+        done();
+      })
+  });
+
+  it('getRequestsByUserId function should return all the requests the user requested', function(done){
+    requestsModel.getRequestsByUserId(user1.id)
+      .then(function(request){
+        expect(request.length).to.equal(1);
+        request = request[0];
+        expect(request.user_id).to.equal(request1.user_id);
+        expect(request.comments).to.equal('Can i join?');
+        expect(request.status).to.equal('pending');
+        done();
+      })
+      .catch(function(err){
+        throw err;
+        console.log("getRequestsByUserId Error: ", err)
         done();
       })
   });
