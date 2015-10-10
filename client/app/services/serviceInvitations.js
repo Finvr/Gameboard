0856797ba348086f32c809e6f3d90331a7432666
Ignacio Prado <ignacio@gatogordoweb.com>
2015-10-10 16:01:20 -0500

    client - v 1.0 features - add Invitations service file

---
+++ client/app/services/serviceInvitations.js
@@ -0,0 +1,20 @@
+(function(){
+
+	angular
+		.module('imgame.service')
+		.factory('Invitations', InvitationsFactory);
+
+	function InvitationsFactory($http){
+		var all = function(){
+			return $http
+								.get('me/invitations')
+								.then(function(data){
+									console.log(data);
+								});
+		}
+		return {
+			all: all
+		};
+	}
+
+})();
