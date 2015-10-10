ef5f712868d661703ebf7938a0aa4f1304139845
Ignacio Prado <ignacio@gatogordoweb.com>
2015-10-10 16:33:26 -0500

    client - karma tests - test http response Invitations.all

--- test/clientTest/invitationsServiceSpec.js
+++ test/clientTest/invitationsServiceSpec.js
@@ -1,14 +1,22 @@
 describe('Invitations service', function(){
-	beforeEach(module(imgame.service));
 
 	var Invitations, http;
+	
+	beforeEach(module('imgame'));
 
-	beforeEach(function(_Invitations_, $httpBackend){
+	beforeEach(inject(function(_Invitations_, $httpBackend){
 		http = $httpBackend;
 		Invitations = _Invitations_;
-	});
+	}));
 
 	it("should be defined", function(){
 		expect(Invitations).toBeDefined();
 	});
-})
+
+	describe("Invigations.all", function(){
+		it("should send a get request to /me/invitations", function(){
+			http.expectGET('/me/invitations').respond(200);
+		});
+	});
+
+});
