0e997d89cd97417bd0f36f94b0c67614402390c2
Ignacio Prado <ignacio@gatogordoweb.com>
2015-10-12 16:15:32 -0500

    client - karma test - add profileCtrlSpec scaffolding

---
+++ test/clientTest/profileCtrlspec.js
@@ -0,0 +1,23 @@
+describe('ProfileController', function(){
+  beforeEach(module('imgame'));
+
+  var ctrl, scope, http, route;
+
+  beforeEach(inject(function($controller, $rootScope, $httpBackend){
+  	scope = $rootScope.$new();
+  	http = $httpBackend;
+
+  	ctrl = $controller('ProfileController', {
+  		$scope: scope,
+      $httpBackend: http,
+      $route: { 
+      		current: {
+      			params: {
+      				id: 1
+      			}
+      		}
+      	}
+  	}); 
+  }));
+
+})
