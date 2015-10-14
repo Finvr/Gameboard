1e72b6ea775a4c953a22b0153760955c41e143e4
Nathan Leonard <n.leonard668@gmail.com>
2015-10-14 12:01:23 -0500

    server-reviews-create functions to calculate reliability and ratings

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -30,4 +30,67 @@ module.exports = {
       res.sendStatus(200);
     });
   }
+
 }
+
+function calculateRatings (reviews) {
+  var userRating = 0;
+  var ratingCount = 0;
+  var reliability;
+  var gameData = {length: 0};
+  // Return default values if not enough reviews
+  if ( reviews.length < 5 ) {
+    return {
+      rating: 5, 
+      reliability: 1, 
+      reviewCount: reviews.length 
+    };
+  }
+
+  rating, showed_up, gameposts_id
+  for ( var i = 0; i < reviews.length; i++ ) {
+    if ( reviews[i].showed_up ) {
+      userRating += reviews[i].rating;
+      ratingCount++;
+    }
+    if ( gameData[reviews[i].gameposts_id] ) {
+      gameData[reviews[i].gameposts_id].push(reviews[i].showed_up);
+    } else {
+      gameData[reviews[i].gameposts_id] = [reviews[i].showed_up];
+      gameData.length++; 
+    }
+  }
+  userRating = userRating/ratingCount;
+  reliability = calulateReliability(gameData);
+  
+  return {
+    rating: userRating,
+    reliability: reliability,
+    reviewCount: reviews.length
+  };
+
+};
+
+function calculateReliability (gameData) {
+  var reliability = 0;
+  var played = gameData.length;
+
+  for ( game in gameData ) {
+    if ( game === 'length' ) continue;
+    if ( gameData[game].length === 1 ) {
+      if ( gameData[game][0] ) {
+        reliability += 1/played;
+      }
+    } else {
+      var trueCount = 0;
+      for ( var i = 0; i < gameData[game].length; i++ ) {
+        if ( gameData[game][i] ) { trueCount++; }
+      }
+      if ( trueCount/gameData[game].length >= 0.5 ) {
+        reliability += 1/played;
+      }
+    }
+  }
+
+  return reliability;
+};
