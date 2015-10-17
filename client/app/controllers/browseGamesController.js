0da87b8c9923194ce033e0a69da4972484a6c2c8
Nathan Leonard <n.leonard668@gmail.com>
2015-10-17 13:46:02 -0500

    client-datepicker-allow users to select weekend days with datepicker

--- client/app/controllers/browseGamesController.js
+++ client/app/controllers/browseGamesController.js
@@ -31,12 +31,11 @@
 		//////////////////////////////////////////////////////////////////
     var currentTime = new Date();
     $scope.startDateFilter = currentTime;
-    //$scope.endDateFilter = currentTime;
-    $scope.month = ['Januar', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    $scope.endDateFilter = currentTime;
+    $scope.month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     $scope.monthShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     $scope.weekdaysFull = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     $scope.weekdaysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-    $scope.disable = [false, 1, 7];
     $scope.today = 'Today';
     $scope.clear = 'Clear';
     $scope.close = 'Close';
