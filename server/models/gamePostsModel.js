90fee4bc7c6ad9269f3a27a049db8acc85a98070
Nathan Leonard <n.leonard668@gmail.com>
2015-10-17 15:07:56 -0500

    comment gamePostsModel.js

--- server/models/gamePostsModel.js
+++ server/models/gamePostsModel.js
@@ -5,6 +5,7 @@ var db       = require('../db.js'),
 module.exports = {
 
   getAll: function (userId, status) {
+    //Get all gameposts, or limit query by userId and/or status
     if (!userId) { var userId = null }; 
     return fetchAllOrByUser(userId, status)
       .then(function (gameposts) {
@@ -17,14 +18,18 @@ module.exports = {
   },
 
   getRecentGames: function(userId) {
+    //Get recently played games for review and populating profile.
     //This function needs to be refactored to use fewer database calls.
     var today = new Date();
     var weekAgo = new Date();
     weekAgo.setDate(weekAgo.getDate()-30);
     var gameposts = [];
+
+    //Get all expired gameposts for the user
     return module.exports.getAll(userId, "expired")
       .then(function(hostGames){
         var games = hostGames;
+        //Get all expired requests for the user
         return Requests.getRequestsByUserId(userId, 'expired')
           .then(function(requests){
             games = games.concat(requests);
@@ -32,7 +37,9 @@ module.exports = {
           })
       })
       .then(function(games){
+        //If the game is in the past, get all of the pictures needed for the page
         for (var i = 0; i < games.length; i ++) {
+          //This date checking should not be necessary
           if ((games[i].game_datetime < today) && (games[i].game_datetime > weekAgo) && (!(games[i].user_id) || games[i].host_id !== games[i].user_id)) {
             if (!games[i].gamepost_id) { games[i].gamepost_id = games[i].id }
             gameposts.push(games[i]);
@@ -59,6 +66,7 @@ module.exports = {
   },
 
   create: function (gamepost) {
+    //Create a gamepost in the database
     return db('gameposts')
       .insert(gamepost)
       .returning("id")
@@ -72,6 +80,7 @@ module.exports = {
   },
 
   deleteGamePost: function (gamepostId, userId) {
+    //Set gamepost status to cancelled
     return db('gameposts')
       .where({
         id: gamepostId,
@@ -85,6 +94,7 @@ module.exports = {
   },
 
   checkForExpired: function () {
+    //Find and update all active gameposts that are in the past to expired
     return db('gameposts')
       .where('post_status', 'active')
       .andWhere('game_datetime', '<', db.raw('now()'))
@@ -97,6 +107,7 @@ module.exports = {
   },
 
   fetchById: function(gamepostId) {
+    //Get a gamepost by ID
     return db('gameposts')
       .where('id', gamepostId)
       .then(function (result) {
@@ -110,10 +121,10 @@ module.exports = {
         return err;
       });
   }
-
 }
 
 function fetchAllOrByUser (userId, status) {
+  //Get all gameposts, or limit query by userId and/or status
   var matchStatus = status;
   var status = status || 'accepted';
   var selectColumns = [
